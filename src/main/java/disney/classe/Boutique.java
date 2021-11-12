c3662a36dea4f0caa132639ba339c1955d18d15f
JustineChampagne <Justine.champagne@outlook.fr>
2021-11-12 17:39:43 +0100

    boutique ok

--- src/main/java/disney/classe/Boutique.java
+++ src/main/java/disney/classe/Boutique.java
@@ -1,5 +1,40 @@
 package disney.classe;
 
-public class Boutique {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Version;
+
+@Entity
+public class Boutique {
+	
+	
+
+	@OneToMany(mappedBy="boutique")
+	private List<Personnage> personnages = new ArrayList<Personnage>();
+	
+	private int vies; 
+	
+	private int nbEtoile;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Version
+	private int version;
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
